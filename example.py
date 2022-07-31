ba851fad4e06a4b971f8e14b1b0b0c974d668f8f
Henrik Schletter <h.schletter168@gmail.com>
2022-07-31 12:50:13 +0200

    fix timedfilehandler to get a more accurate rollover time.

--- example.py
+++ example.py
@@ -2,12 +2,20 @@ import CustomLogger
 import example_module
 from time import sleep
 
-log = CustomLogger.getLogger()
+log = CustomLogger.getLogger(level=10)
+
+
+def next_rollover():
+    import datetime
+    last_rollover = datetime.datetime.now()
+    next_day = last_rollover + datetime.timedelta(days=1)
+    return datetime.datetime(next_day.year, next_day.month, next_day.day)
 
 
 def main():
+    print(next_rollover())
     example_module.test()
-    for i in range(3):
+    for i in range(10):
         log.info(f"Test {i}")
         sleep(1)
 
